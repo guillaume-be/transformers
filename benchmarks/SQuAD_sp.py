587c6a2578beee5a48ed90ab0379c7d19bb2565e
Guillaume B <guillaume.becquin@gmail.com>
2020-07-21 19:27:41 +0200

    Update of SST2 and SQuAD benchmarks

--- benchmarks/SQuAD_sp.py
+++ benchmarks/SQuAD_sp.py
@@ -1,7 +1,7 @@
 import time
 
 from benchmarks.qa_utils import decode, generate_batch_indices, inputs_for_model
-from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
+from transformers import BertTokenizerFast, BertForQuestionAnswering, BertTokenizer
 from transformers.data.processors.squad import squad_convert_examples_to_features, SquadV2Processor
 from pathlib import Path
 from tqdm import tqdm
@@ -23,19 +23,20 @@ if __name__ == '__main__':
     topk = 1
     max_answer_len = 15
     batch_size = 64
+    n_iter = 10
 
-    t1 = time.time()
-    print(f'Set-up: {(t1 - t0) * 1000}ms')
+    loading_times = []
+    forward_pass_times = []
 
     # ========================
     # Initialization
     # ========================
-
-    tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
-    model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad").cuda()
-
-    t2 = time.time()
-    print(f'initialization: {(t2 - t1)}s')
+    for i in range(n_iter):
+        t1 = time.time()
+        tokenizer = BertTokenizer.from_pretrained("bert-large-cased-whole-word-masking-finetuned-squad")
+        model = BertForQuestionAnswering.from_pretrained("bert-large-cased-whole-word-masking-finetuned-squad").cuda()
+        t2 = time.time()
+        loading_times.append(t2 - t1)
 
     # ========================
     # Pre-processing
@@ -44,92 +45,88 @@ if __name__ == '__main__':
     processor = SquadV2Processor()
     data = processor.get_dev_examples(root_path)
 
-    t3 = time.time()
-    print(f'Loading examples: {(t3 - t2)}s')
+    for i in range(n_iter):
+        t1 = time.time()
 
-    features = squad_convert_examples_to_features(
-        examples=data,
-        tokenizer=tokenizer,
-        max_seq_length=max_seq_length,
-        doc_stride=doc_stride,
-        max_query_length=max_query_length,
-        is_training=False,
-        threads=1
-    )
+        features = squad_convert_examples_to_features(
+            examples=data,
+            tokenizer=tokenizer,
+            max_seq_length=max_seq_length,
+            doc_stride=doc_stride,
+            max_query_length=max_query_length,
+            is_training=False,
+            threads=1
+        )
 
-    batch_indices = generate_batch_indices(features, batch_size)
+        batch_indices = generate_batch_indices(features, batch_size)
 
-    t4 = time.time()
-    print(f'Pre-processing: {(t4 - t3)}s')
+        # ========================
+        # Forward pass
+        # ========================
 
-    # ========================
-    # Forward pass
-    # ========================
+        all_answers = []
+        forward_time = []
+        postprocessing_time = []
+        for (start, end) in tqdm(batch_indices):
+            t_batch = time.time()
+            batch_features = features[start: end]
+            fw_args = inputs_for_model([f.__dict__ for f in batch_features])
 
-    all_answers = []
-    forward_time = []
-    postprocessing_time = []
-    for (start, end) in tqdm(batch_indices):
-        t_batch = time.time()
-        batch_features = features[start: end]
-        fw_args = inputs_for_model([f.__dict__ for f in batch_features])
+            with torch.no_grad():
+                # Retrieve the score for the context tokens only (removing question tokens)
+                fw_args = {k: torch.tensor(v).cuda() for (k, v) in fw_args.items()}
+                start, end = model(**fw_args)
+                start, end = start.cpu().numpy(), end.cpu().numpy()
+            t_forward = time.time()
+            forward_time.append(t_forward - t_batch)
 
-        with torch.no_grad():
-            # Retrieve the score for the context tokens only (removing question tokens)
-            fw_args = {k: torch.tensor(v).cuda() for (k, v) in fw_args.items()}
-            start, end = model(**fw_args)
-            start, end = start.cpu().numpy(), end.cpu().numpy()
-        t_forward = time.time()
-        forward_time.append(t_forward - t_batch)
+            example_to_feature_ids = dict()
+            for feature_index, feature in enumerate(batch_features):
+                example_to_feature_ids[feature.example_index] = feature_index + 1
 
-        example_to_feature_ids = dict()
-        for feature_index, feature in enumerate(batch_features):
-            example_to_feature_ids[feature.example_index] = feature_index + 1
+            feature_id_start = 0
 
-        feature_id_start = 0
+            for (example_id, max_feature_id) in example_to_feature_ids.items():
+                answers = []
+                for example_feature_id in range(feature_id_start, max_feature_id):
+                    feature = batch_features[example_feature_id]
+                    start_ = start[example_feature_id]
+                    end_ = end[example_feature_id]
+                    example = data[example_id]
+                    # Normalize logits and spans to retrieve the answer
+                    start_ = np.exp(start_) / np.sum(np.exp(start_))
+                    end_ = np.exp(end_) / np.sum(np.exp(end_))
 
-        for (example_id, max_feature_id) in example_to_feature_ids.items():
-            answers = []
-            for example_feature_id in range(feature_id_start, max_feature_id):
-                feature = batch_features[example_feature_id]
-                start_ = start[example_feature_id]
-                end_ = end[example_feature_id]
-                example = data[example_id]
-                # Normalize logits and spans to retrieve the answer
-                start_ = np.exp(start_) / np.sum(np.exp(start_))
-                end_ = np.exp(end_) / np.sum(np.exp(end_))
+                    # Mask padding and question
+                    start_, end_ = (
+                        start_ * np.abs(np.array(feature.p_mask) - 1),
+                        end_ * np.abs(np.array(feature.p_mask) - 1),
+                    )
 
-                # Mask padding and question
-                start_, end_ = (
-                    start_ * np.abs(np.array(feature.p_mask) - 1),
-                    end_ * np.abs(np.array(feature.p_mask) - 1),
-                )
+                    # TODO : What happens if not possible
+                    # Mask CLS
+                    start_[0] = end_[0] = 0
 
-                # TODO : What happens if not possible
-                # Mask CLS
-                start_[0] = end_[0] = 0
+                    starts, ends, scores = decode(start_, end_, topk, max_answer_len)
+                    char_to_word = np.array(example.char_to_word_offset)
+                    # Convert the answer (tokens) back to the original text
+                    answers += [
+                        {
+                            "score": score.item(),
+                            "start": np.where(char_to_word == feature.token_to_orig_map[s])[0][0].item(),
+                            "end": np.where(char_to_word == feature.token_to_orig_map[e])[0][-1].item(),
+                            "answer": " ".join(
+                                example.doc_tokens[feature.token_to_orig_map[s]: feature.token_to_orig_map[e] + 1]
+                            ),
+                        }
+                        for s, e, score in zip(starts, ends, scores)
+                    ]
+                    feature_id_start = max_feature_id
+                answers = sorted(answers, key=lambda x: x["score"], reverse=True)[: topk]
+                all_answers += answers
+            postprocessing_time.append(time.time() - t_forward)
+        t2 = time.time()
+        forward_pass_times.append(t2 - t1)
 
-                starts, ends, scores = decode(start_, end_, topk, max_answer_len)
-                char_to_word = np.array(example.char_to_word_offset)
-                # Convert the answer (tokens) back to the original text
-                answers += [
-                    {
-                        "score": score.item(),
-                        "start": np.where(char_to_word == feature.token_to_orig_map[s])[0][0].item(),
-                        "end": np.where(char_to_word == feature.token_to_orig_map[e])[0][-1].item(),
-                        "answer": " ".join(
-                            example.doc_tokens[feature.token_to_orig_map[s]: feature.token_to_orig_map[e] + 1]
-                        ),
-                    }
-                    for s, e, score in zip(starts, ends, scores)
-                ]
-                feature_id_start = max_feature_id
-            answers = sorted(answers, key=lambda x: x["score"], reverse=True)[: topk]
-            all_answers += answers
-        postprocessing_time.append(time.time() - t_forward)
-
-    t5 = time.time()
-    print(f'Inference: {(t5 - t4)}s')
-    print(f'Inference (forward): {sum(forward_time)}s')
-    print(f'Inference (post-processing): {sum(postprocessing_time)}s')
-    print(f'Total: {(t5 - t0)}s')
+    print(f'Inference: {sum(forward_pass_times) / len(forward_pass_times)}s')
+    print(f'Total: {sum(loading_times) / len(loading_times)}s')
--- benchmarks/SST2_pipeline.py
+++ benchmarks/SST2_pipeline.py
@@ -18,18 +18,22 @@ if __name__ == '__main__':
     root_path = Path('E:/Coding/data-resources/sst2/')
     vocab_path = Path('E:/Coding/cache/rustbert/distilbert-sst2/vocab.txt')
     batch_size = 64
+    n_iter = 10
 
-    t1 = time.time()
-    print(f'Set-up: {(t1 - t0) * 1000}ms')
+    loading_times = []
+    forward_pass_times = []
 
     # ========================
     # Initialization
     # ========================
 
-    tokenizer = DistilBertTokenizerFast.from_pretrained(str(vocab_path))
-    model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english").cuda()
-    t2 = time.time()
-    print(f'initialization: {(t2 - t1)}s')
+    for i in range(n_iter):
+        t1 = time.time()
+        tokenizer = DistilBertTokenizerFast.from_pretrained(str(vocab_path))
+        model = AutoModelForSequenceClassification.from_pretrained(
+            "distilbert-base-uncased-finetuned-sst-2-english").cuda()
+        t2 = time.time()
+        loading_times.append(t2 - t1)
 
     # ========================
     # Pre-processing
@@ -37,22 +41,23 @@ if __name__ == '__main__':
 
     processor = Sst2Processor()
     data = processor.get_train_examples(root_path)
-    data = [example.text_a for example in data]
+    data = [example.text_a for example in data[:5000]]
 
     # ========================
     # Pipeline execution
     # ========================
+    for i in range(n_iter):
+        t1 = time.time()
+        num_batches = len(data) // batch_size + 1
+        outputs = []
+        for batch_index in range(num_batches):
+            batch = data[batch_index * batch_size:min((batch_index + 1) * batch_size, len(data))]
+            inputs = tokenizer.batch_encode_plus(batch, return_tensors='pt', padding=True)['input_ids'].cuda(0)
+            with torch.no_grad():
+                model_outputs = model(inputs)[0].detach().cpu()
+            outputs.extend(model_outputs.softmax(dim=-1))
+        t2 = time.time()
+        forward_pass_times.append(t2 - t1)
 
-    num_batches = len(data) // batch_size + 1
-    outputs = []
-    for batch_index in range(num_batches):
-        batch = data[batch_index * batch_size:min((batch_index + 1) * batch_size, len(data))]
-        inputs = tokenizer.batch_encode_plus(batch, return_tensors='pt')['input_ids'].cuda(0)
-        with torch.no_grad():
-            model_outputs = model(inputs)[0].detach().cpu()
-        outputs.extend(model_outputs.softmax(dim=-1))
-
-    print(len(outputs))
-    t3 = time.time()
-    print(f'Inference: {(t3 - t2)}s')
-    print(f'Total: {(t3 - t0)}s')
+    print(f'Inference: {sum(forward_pass_times) / len(forward_pass_times)}s')
+    print(f'Total: {sum(loading_times) / len(loading_times)}s')
